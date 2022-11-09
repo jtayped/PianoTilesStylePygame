7de6483e3dfedaf2b851d6e3c75fe1af690e1e54
jtayped <87146558+jtayped@users.noreply.github.com>
2022-11-09 13:54:09 +0100

    restart

--- files/level.py
+++ files/level.py
@@ -1,97 +1,33 @@
-import pygame, random
-from files.settings import *
 from files.tile import Tile
+from files.settings import *
+import pygame, random
 
 class Level:
     def __init__(self) -> None:
         pygame.init()
-
+        pygame.mixer.init() 
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
-        pygame.display.set_caption(str(round(self.clock.get_fps(), 1)))
-    
-        self.font = pygame.font.SysFont('AGENCYB.TTF', 75)
-        self.tinyFont = pygame.font.SysFont('AGENCYB.TTF', 25)
 
-        self.running = True
+        pygame.display.set_caption(f"{self.clock.get_fps()} fps")
+        self.nColumns = 4
 
-        self.tilesInX = 4
         self.tileList = []
-        self.tileSize = [int(WIDTH/self.tilesInX), HEIGHT/7]
-        self.tileBuffer = HEIGHT/self.tileSize[1]*2.5
+        self.tileSize = [WIDTH/self.nColumns, HEIGHT/15]
+        self.createTile(True)
 
-        self.tileSpeed = 2
-        self.tileColor = 'grey12'
-
-        pos = [random.randint(0, self.tilesInX-1)*self.tileSize[0], -self.tileSize[1]]
-        self.tileList.append(Tile(self.screen, pos, self.tileSize, self.tileColor, self.tileSpeed, False))
-
-        self.mouseButtonDown = False
-    
-    def generateTile(self):
+    def getNewTilePos(self):
         pos = []
         size = self.tileSize[:]
 
-        pos.append(random.choice([0, 1, 2, 3])*size[0])
-        pos.append(self.tileList[-1].rect.y - self.tileList[-1].rect.height)
+        if len(self.tileList) == 0:
+            pos.append(0)
 
-        long = False
-        if random.randint(0, 5) == 4:
-            size[1] *= 2
-            long = True
+        else:
+            pos.append(random.choice([0, 1, 2, 3]*size[0]))
 
-        self.tileList.append(Tile(self.screen, pos, size, self.tileColor, self.tileSpeed, long))
+        return pos
 
-    def tileUpdate(self):
-        for tile in self.tileList:
-            tile.update()
-
-            if tile.offScreen():
-                self.tileList.remove(tile)
-
-            if self.mouseButtonDown and tile.checkClick(self.mouseClick):
-                tile.clicked = True
-            
-            elif not self.mouseButtonDown:
-                tile.clicked = False
-
-    def tileHandler(self):
-        self.tileUpdate()
-
-        if len(self.tileList) < self.tileBuffer:
-            self.generateTile()
-
-    def getMouseHold(self):
-        if self.mouseButtonDown:
-            self.mouseClick = pygame.mouse.get_pos()
-
-    def events(self):
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT:
-                self.running = False
-
-            if event.type == pygame.MOUSEBUTTONDOWN: 
-                self.mouseButtonDown = True
-            
-            elif event.type == pygame.MOUSEBUTTONUP:
-                self.mouseButtonDown = False
-
-    def update(self):
-        self.clock.tick(FPS)
-        self.events()
-        self.screen.fill('white')
-        self.fps = self.clock.get_fps()
-        pygame.display.set_caption(str(round(self.fps)))
-        
-        ########################################
-
-        self.getMouseHold()
-        self.tileHandler()
-
-        ########################################
-
-        pygame.display.flip()    
-
-    def run(self):
-        while self.running:
-            self.update()
+    def createTile(self, start=False):
+        if start:
+            pos = self.getNewTilePos()
--- files/tile.py
+++ files/tile.py
@@ -1,55 +1,23 @@
-from files.settings import *
 import pygame
+from files.settings import *
 
 class Tile:
-    def __init__(self, surface, pos, size, color, speed, long) -> None:
+    def __init__(self, surface, pos, size, color, speed) -> None:
         self.screen = surface
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
+
         self.color = color
         self.speed = speed
-        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
-        self.long = long
-
-        self.circleCenter = [self.rect.x + self.rect.width/2, self.rect.y + self.rect.height*0.8]
-        self.circleRadius = WIDTH/45
-
-        self.clicked = False
-        self.circleDisplace = 0
-
-        self.top = self.rect.y + self.rect.height*0.2
-        self.dead = False
-
+    
     def offScreen(self):
-        return self.rect.top > HEIGHT*1.5
-
-    def checkClick(self, point):
-        if self.rect.collidepoint(point):
-            self.clicked = True
-
-    def checkDead(self):
-        if self.circleCenter[1] < self.top and self.long:
-            self.dead = True
-            self.color = 'green'
+        return self.rect.y > HEIGHT*1.5
 
     def movement(self):
         self.rect.y += self.speed
-        if not self.clicked and self.long and not self.dead:
-            self.circleCenter = [self.rect.x + self.rect.width/2, self.rect.y + self.rect.height*0.8 + self.circleDisplace]
-        else:
-            self.circleDisplace -= self.speed
-
+    
     def draw(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
-
-        if self.long:
-            pygame.draw.circle(self.screen, 'grey', self.circleCenter, self.circleRadius, 1)
-            pygame.draw.line(self.screen, 'grey', 
-            (self.circleCenter[0], self.top),
-            [self.rect.x + self.rect.width/2, self.rect.y + self.rect.height*0.8],
-            1
-            )
     
     def update(self):
-        self.top += self.speed
         self.movement()
-        self.checkDead()
         self.draw()
